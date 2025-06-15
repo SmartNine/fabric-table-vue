3d2d71bb285578f6a9441bae5eec53135bb66107
SmartNine <realsmartnine@gmail.com>
2025-06-15 01:06:19 -0700

    add functions

--- src/components/FabricTable.vue
+++ src/components/FabricTable.vue
@@ -1,29 +1,36 @@
 <template>
   <div style="position: relative">
-    <div class="toolbar">
-      <button @click="addRow">➕ 添加行</button>
-      <button @click="addCol">➕ 添加列</button>
-      <button @click="removeRow">➖ 删除行</button>
-      <button @click="removeCol">➖ 删除列</button>
-    </div>
+    <RowHeader :rows="table.length" :cellHeight="props.cellHeight" :tableTop="tableTop"
+      :totalHeight="table.length * props.cellHeight" :selectedRow="selectedRowIndex"
+      @select-row="handleRowHeaderClick" />
+
+    <ColHeader :cols="table[0]?.length || 0" :cellWidth="props.cellWidth" :tableLeft="tableLeft"
+      :totalWidth="table[0]?.length * props.cellWidth || 0" :selectedCol="selectedColIndex"
+      @select-col="handleColHeaderClick" />
 
     <canvas ref="canvasEl" :width="canvasWidth" :height="canvasHeight" />
 
     <!-- 行工具栏 -->
-    <RowToolbar v-if="selectedRowIndex !== null" :top="toolbarTop" @insert-above="insertRowAbove"
-      @insert-below="insertRowBelow" @delete="deleteSelectedRow" />
+    <RowToolbar v-if="showRowToolbar" :top="toolbarTop" @insert-above="insertRowAbove" @insert-below="insertRowBelow"
+      @delete="deleteSelectedRow" />
 
     <!-- 列工具栏 -->
-    <ColToolbar v-if="selectedColIndex !== null" :top="20" :left="toolbarLeft" @insert-left="insertColLeft"
+    <ColToolbar v-if="showColToolbar" :top="20" :left="toolbarLeft" @insert-left="insertColLeft"
       @insert-right="insertColRight" @delete="deleteSelectedCol" @align="setColAlign" />
+
+    <!-- 浮动输入框 -->
+    <input v-if="showInput" v-model="inputValue" :style="inputStyle" class="floating-input" @blur="applyInput"
+      @keyup.enter="applyInput" />
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted, defineProps, defineExpose } from 'vue'
+import { ref, onMounted, defineProps, defineExpose, watch, nextTick } from 'vue'
 import { fabric } from 'fabric'
 import RowToolbar from './RowToolbar.vue'
 import ColToolbar from './ColToolbar.vue'
+import RowHeader from './RowHeader.vue'
+import ColHeader from './ColHeader.vue'
 
 const props = defineProps({
   rows: { type: Number, default: 3 },
@@ -38,11 +45,39 @@ const canvasWidth = 1000
 const canvasHeight = 600
 let canvas, tableGroup
 const table = ref([])
-const textRefs = ref([]) // 记录每格 IText 对象
+const textRefs = ref([])
+const rectRefs = ref([])
 const selectedRowIndex = ref(null)
 const selectedColIndex = ref(null)
 const toolbarTop = ref(0)
 const toolbarLeft = ref(0)
+const tableTop = 100
+const tableLeft = 100
+const showRowToolbar = ref(false)
+const showColToolbar = ref(false)
+
+// 输入框相关
+const showInput = ref(false)
+const inputValue = ref('')
+const inputStyle = ref({})
+let editingText = null
+
+watch([selectedRowIndex, selectedColIndex], () => {
+  showRowToolbar.value = selectedRowIndex.value !== null
+  showColToolbar.value = selectedColIndex.value !== null
+})
+
+function highlightRowCol() {
+  for (let r = 0; r < rectRefs.value.length; r++) {
+    for (let c = 0; c < rectRefs.value[r].length; c++) {
+      const rect = rectRefs.value[r][c]
+      const isRow = selectedRowIndex.value === r
+      const isCol = selectedColIndex.value === c
+      rect.set('fill', isRow || isCol ? '#e6f4ff' : '#fff')
+    }
+  }
+  canvas.requestRenderAll()
+}
 
 function initTableData(rows, cols, data = []) {
   const result = []
@@ -59,12 +94,14 @@ function initTableData(rows, cols, data = []) {
 function renderTable() {
   canvas.clear()
   textRefs.value = []
+  rectRefs.value = []
   const cells = []
   const rows = table.value.length
   const cols = table.value[0].length
 
   for (let r = 0; r < rows; r++) {
     const textRow = []
+    const rectRow = []
     for (let c = 0; c < cols; c++) {
       const left = c * props.cellWidth
       const top = r * props.cellHeight
@@ -80,37 +117,28 @@ function renderTable() {
         selectable: false,
       })
 
-      const text = new fabric.IText(table.value[r][c], {
+      const text = new fabric.Textbox(table.value[r][c], {
         left: left + 5,
         top: top + 5,
         fontSize: 16,
         width: props.cellWidth - 10,
         height: props.cellHeight - 10,
-        editable: true,
+        editable: false,
         selectable: true,
         textAlign: 'center',
       })
 
-      text.on('mousedown', () => {
-        selectedRowIndex.value = r
-        selectedColIndex.value = c
-        toolbarTop.value = top + 20
-        toolbarLeft.value = left + 20
-      })
-
-      text.on('changed', () => {
-        table.value[r][c] = text.text
-      })
-
       textRow.push(text)
+      rectRow.push(rect)
       cells.push(rect, text)
     }
     textRefs.value.push(textRow)
+    rectRefs.value.push(rectRow)
   }
 
   tableGroup = new fabric.Group(cells, {
-    left: 100,
-    top: 100,
+    left: tableLeft,
+    top: tableTop,
     subTargetCheck: true,
     hasControls: true,
     hasBorders: true,
@@ -119,6 +147,27 @@ function renderTable() {
   canvas.add(tableGroup)
   canvas.setActiveObject(tableGroup)
   canvas.renderAll()
+}
+
+function applyInput() {
+  if (editingText && selectedRowIndex.value !== null && selectedColIndex.value !== null) {
+    editingText.text = inputValue.value
+    table.value[selectedRowIndex.value][selectedColIndex.value] = inputValue.value
+    showInput.value = false
+    canvas.requestRenderAll()
+  }
+}
+
+function handleRowHeaderClick(index) {
+  selectedRowIndex.value = index
+  toolbarTop.value = tableTop + index * props.cellHeight + 20
+  highlightRowCol()
+}
+
+function handleColHeaderClick(index) {
+  selectedColIndex.value = index
+  toolbarLeft.value = tableLeft + index * props.cellWidth + 20
+  highlightRowCol()
 }
 
 function addRow() {
@@ -142,7 +191,6 @@ function removeCol() {
   renderTable()
 }
 
-// 行操作
 function insertRowAbove() {
   if (selectedRowIndex.value !== null) {
     table.value.splice(selectedRowIndex.value, 0, Array(table.value[0].length).fill(''))
@@ -162,7 +210,6 @@ function deleteSelectedRow() {
   }
 }
 
-// 列操作
 function insertColLeft() {
   if (selectedColIndex.value !== null) {
     for (let row of table.value) row.splice(selectedColIndex.value, 0, '')
@@ -197,6 +244,47 @@ onMounted(() => {
   canvas = new fabric.Canvas(canvasEl.value, { selection: true })
   table.value = initTableData(props.rows, props.cols, props.tableData)
   renderTable()
+
+  canvas.on('mouse:down', (opt) => {
+    const target = opt.target
+    if (!target || target === tableGroup) {
+      selectedRowIndex.value = null
+      selectedColIndex.value = null
+      showInput.value = false
+      highlightRowCol()
+      canvas.requestRenderAll()
+    }
+  })
+
+  canvas.on('mouse:dblclick', (opt) => {
+    const target = opt.target
+    if (target && target.type === 'textbox') {
+      const r = Math.floor(target.top / props.cellHeight)
+      const c = Math.floor(target.left / props.cellWidth)
+      selectedRowIndex.value = r
+      selectedColIndex.value = c
+      highlightRowCol()
+
+      editingText = textRefs.value[r][c] // 使用已缓存的 TextBox 引用
+      inputValue.value = editingText.text
+
+      inputStyle.value = {
+        position: 'absolute',
+        left: `${tableLeft + editingText.left}px`,
+        top: `${tableTop + editingText.top}px`,
+        width: `${editingText.width}px`,
+        height: `${props.cellHeight - 10}px`,
+        fontSize: '16px',
+        padding: '2px 4px',
+        border: '1px solid #888',
+      }
+
+      showInput.value = true
+      nextTick(() => {
+        document.querySelector('.floating-input')?.focus()
+      })
+    }
+  })
 })
 
 defineExpose({
@@ -212,11 +300,19 @@ canvas {
 
 .toolbar {
   margin-bottom: 10px;
+  display: none;
 }
 
 button {
   margin-right: 10px;
   padding: 6px 10px;
   font-size: 14px;
+}
+
+.floating-input {
+  position: absolute;
+  z-index: 10;
+  box-sizing: border-box;
+  outline: none;
 }
 </style>
